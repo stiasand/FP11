96abe0210ea5f4ac9b8f6b6eed5d3661f839de21
john_edvard <john_edvard@dhcp-105-238.idi.ntnu.no>
2011-03-08 12:56:27 +0100

    Removed some comments

--- src/client/gui/Config.java
+++ src/client/gui/Config.java
@@ -5,5 +5,4 @@ import java.io.Serializable;
 class Config implements Serializable {
 	public static final String frameTitle = "Wonderful Calendar";
 	public static final int frameWidth = 800, frameHeight = 600;
-	//dette vil jeg også ha med!.
 }
